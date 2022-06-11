fdae1a8c5f91b951f7c463a98b5b98ff26a0a4db
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-06-12 00:27:11 +0530

    Update userinfo.py

--- KaguyaRobot/modules/userinfo.py
+++ KaguyaRobot/modules/userinfo.py
@@ -447,7 +447,7 @@ def set_about_me(update: Update, context: CallbackContext):
 def stats(update: Update, context: CallbackContext):
     stats = "<b>╔═━「 Current Kaguya Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 Powered By Shiinobu 」</b>"
+    result += "\n<b>╘═━「 Powered By Hellsing 」</b>"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML, 
