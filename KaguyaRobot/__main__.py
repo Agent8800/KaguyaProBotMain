f0e9d6267373efc02f7e1748f8eaf38347a1c218
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-06-18 20:16:40 +0530

    Update __main__.py

--- KaguyaRobot/__main__.py
+++ KaguyaRobot/__main__.py
@@ -82,7 +82,8 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-[やあ]({}) "O Kawaii Koto" {} - San!
+────「 [Kaguya Shinomiya]({}) 」────
+やあ "O Kawaii Koto" {} - San!
 I'm Vice-President Of Council Here To Manage Your Group With Useful & Advanced Features 
 
 ◈ Version : 3.10.6
