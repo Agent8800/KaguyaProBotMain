639792a19a1c9e0b4185421de86709f65a66408c
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-06-22 19:02:55 +0530

    Update xelcius.py

--- KaguyaRobot/modules/xelcius.py
+++ KaguyaRobot/modules/xelcius.py
@@ -1,4 +1,6 @@
+__mod_name__ = "ReqGban"
+
 __help__ ="""
   ──「 Gban Request 」──
-• `/reqgban`*:* <Username/ID> will request a gban to our support staff"""
-__mod_name__ = "ReqGban"
+• `/reqgban`*:* <Username/ID> will request a gban to our support staff
+"""
