deaa7986dc18cc0b20aa1af7a04356126030fd6f
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-06-22 23:35:09 +0530

    Update xelcius.py

--- KaguyaRobot/modules/xelcius.py
+++ KaguyaRobot/modules/xelcius.py
@@ -1,6 +1,13 @@
+from KaguyaRobot.modules.language import gs
+
+
+def helps(chat):
+    return gs(chat, "reqgban_help")
+
 __mod_name__ = "ReqGban"
 
 __help__ ="""
-  ──「 Gban Request 」──
+──「 Gban Request 」──
 • `/reqgban`*:* <Username/ID> will request a gban to our support staff
+
 """
