1733a4a7d7378218434457a24587bbb2f3a90303
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-06-23 02:24:20 +0530

    Update bans.py

--- KaguyaRobot/modules/bans.py
+++ KaguyaRobot/modules/bans.py
@@ -510,20 +510,24 @@ def selfunban(context: CallbackContext, update: Update) -> str:
 
         
 __help__ = """
-  ──「 BANS/MUTE 」──
- • /punchme*:* punchs the user who issued the command
+*User Commands:*
+❂ /kickme*:* kicks the user who issued the command
 *Admins only:*
- • /ban <userhandle>*:* bans a user. (via handle, or reply)
- • /sban <userhandle>*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
- • /tban <userhandle> x(m/h/d)*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • /unban <userhandle>*:* unbans a user. (via handle, or reply)
- • /punch <userhandle>*:* Punches a user out of the group, (via handle, or reply)
- *Admins only:*
- • /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
- • /tmute <userhandle> x(m/h/d)*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • /unmute <userhandle>*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+❂ /ban <userhandle>*:* bans a user. (via handle, or reply)
+❂ /sban <userhandle>*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
+❂ /tban <userhandle> x(m/h/d)*:* bans a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+❂ /unban <userhandle>*:* unbans a user. (via handle, or reply)
+❂ /kick <userhandle>*:* kicks a user out of the group, (via handle, or reply)
+❂ /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
+❂ /tmute <userhandle> x(m/h/d)*:* mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+❂ /unmute <userhandle>*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+❂ /zombies*:* searches deleted accounts
+❂ /zombies clean*:* removes deleted accounts from the group.
+❂ /snipe <chatid> <string>*:* Make me send a message to a specific chat.
 """
-__mod_name__ = "Bans/Mutes"
+
+
+__mod_name__ = "Bans/Mutes
 
 BAN_HANDLER = CommandHandler(["ban", "sban"], ban)
 TEMPBAN_HANDLER = CommandHandler(["tban"], temp_ban)
