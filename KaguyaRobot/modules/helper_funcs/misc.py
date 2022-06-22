d7a47d0a0211cd617ef5e31b1ca7eb8a151360f8
Xelcius <97951640+RimuruDemonlord@users.noreply.github.com>
2022-06-22 09:05:51 -0400

    Update misc.py

--- KaguyaRobot/modules/helper_funcs/misc.py
+++ KaguyaRobot/modules/helper_funcs/misc.py
@@ -1,7 +1,10 @@
+from  uuid  import  uuid4
+
+from math import ceil
 from typing import Dict, List
 
 from KaguyaRobot import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
+from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, InlineQueryResultArticle, InputTextMessageContent
 from telegram.error import TelegramError
 
 
@@ -29,9 +32,8 @@ def split_message(msg: str) -> List[str]:
         else:
             result.append(small_msg)
             small_msg = line
-    else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
+    # Else statement at the end of the for loop, so append the leftover string.
+    result.append(small_msg)
 
     return result
 
@@ -79,7 +81,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="Kaguya_back"),
+                EqInlineKeyboardButton("Back", callback_data="Kaguya_back"),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
@@ -87,9 +89,31 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="Kaguya_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="Kaguya_back")]]
 
     return pairs
+
+
+def article(
+    title: str = "",
+    description: str = "",
+    message_text: str = "",
+    thumb_url: str = None,
+    reply_markup: InlineKeyboardMarkup = None,
+    disable_web_page_preview: bool = False,
+) -> InlineQueryResultArticle:
+
+    return InlineQueryResultArticle(
+        id=uuid4(),
+        title=title,
+        description=description,
+        thumb_url=thumb_url,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=disable_web_page_preview,
+        ),
+        reply_markup=reply_markup,
+    )
 
 def send_to_list(
     bot: Bot, send_to: list, message: str, markdown=False, html=False
@@ -120,14 +144,12 @@ def build_keyboard(buttons):
 
 
 def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
+    return "".join(
+        "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
+        if btn.same_line
+        else "\n[{}](buttonurl://{})".format(btn.name, btn.url)
+        for btn in buttons
+    )
 
 
 def build_keyboard_parser(bot, chat_id, buttons):
@@ -145,41 +167,3 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
 def is_module_loaded(name):
     return name not in NO_LOAD
-
-def convert_gif(input):
-    """Function to convert mp4 to webm(vp9)"""
-
-    vid = cv2.VideoCapture(input)
-    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-
-    #check height and width to scale
-    if width > height:
-        width = 512
-        height = -1
-    elif height > width:
-        height = 512
-        width = -1
-    elif width == height:
-        width = 512
-        height = 512
-
-
-    converted_name = 'kangsticker.webm'
-
-    (
-        ffmpeg
-            .input(input)
-            .filter('fps', fps=30, round="up")
-            .filter('scale', width=width, height=height)
-            .trim(start="00:00:00", end="00:00:03", duration="3")
-            .output(converted_name, vcodec="libvpx-vp9", 
-                        **{
-                            #'vf': 'scale=512:-1',
-                            'crf': '30'
-                            })
-            .overwrite_output()
-            .run()
-    )
-
-    return converted_name
