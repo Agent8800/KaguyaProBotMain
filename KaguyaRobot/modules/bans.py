13b19c14e3fe944d4c22c90e51125132a2fe34da
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-06-24 20:36:11 +0530

    Update bans.py

--- KaguyaRobot/modules/bans.py
+++ KaguyaRobot/modules/bans.py
@@ -170,9 +170,8 @@ def ban(update: Update, context: CallbackContext) -> str:
                     ]
                 ]
             ),
-            parse_mode=ParseMode.HTML,
-        
-        )
+            parse_mode=ParseMode.HTML,        
+                )
         return log
 
     except BadRequest as excp:
