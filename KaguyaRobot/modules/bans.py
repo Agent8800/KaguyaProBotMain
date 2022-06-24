0ba9f3a4a83ae84805c752845dc32af75f6f3a2f
Xelcius <97951640+RimuruDemonlord@users.noreply.github.com>
2022-06-24 11:19:05 -0400

    Update bans.py

--- KaguyaRobot/modules/bans.py
+++ KaguyaRobot/modules/bans.py
@@ -170,9 +170,8 @@ def ban(update: Update, context: CallbackContext) -> str:
                     ]
                 ]
             ),
-            parse_mode=ParseMode.HTML,        
-                )
-        return log
+            parse_mode=ParseMode.HTML,       
+            return log
 
     except BadRequest as excp:
         if excp.message == "Reply message not found":
