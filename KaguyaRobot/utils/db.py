9799eaf1b3241dfe2179dae497b9abccc78527f7
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-06-27 11:58:50 +0530

    Create db.py

---
+++ KaguyaRobot/utils/db.py
@@ -0,0 +1,27 @@
+__all__ = ['get_collection']
+
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from motor.core import AgnosticClient, AgnosticDatabase, AgnosticCollection
+from KaguyaRobot import MONGO_DB_URI as DB_URL
+
+print("Connecting to Database ...")
+
+_MGCLIENT: AgnosticClient = AsyncIOMotorClient(DB_URL)
+_RUN = asyncio.get_event_loop().run_until_complete
+
+if "ErzaMongo" in _RUN(_MGCLIENT.list_database_names()):
+    print("ErzaMongo Database Found :) => Now Logging to it...")
+else:
+    print("ErzaMongo Database Not Found :( => Creating New Database...")
+
+_DATABASE: AgnosticDatabase = _MGCLIENT["Erza"]
+
+
+def get_collection(name: str) -> AgnosticCollection:
+    """ Create or Get Collection from your database """
+    return _DATABASE[name]
+
+
+def _close_db() -> None:
+    _MGCLIENT.close()
