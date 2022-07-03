4ca095eff3ca5c179722039dc20847edc69526c7
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-07-04 00:43:51 +0530

    Update config.py

--- KaguyaRobot/config.py
+++ KaguyaRobot/config.py
@@ -75,7 +75,7 @@ class Config(object):
     SPAMMERS = None
     REM_BG_API_KEY = "xYCR1ZyK3ZsofjH7Y6hPcyzC"
     OPENWEATHERMAP_ID = "887da2c60d9f13fe78b0f9d0c5cbaade"
-    TRIGGERS = / !
+    TRIGGERS = "/ !"
 
 
 class Production(Config):
