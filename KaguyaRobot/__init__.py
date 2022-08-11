ac36428c34dd58980db8e6cda502e5f90ffb5177
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <contact.sohail.me@gmail.com>
2022-08-11 16:14:32 -0700

    Update __init__.py

--- KaguyaRobot/__init__.py
+++ KaguyaRobot/__init__.py
@@ -260,7 +260,6 @@ pbot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=TOKEN,
-    plugins=dict(root="KaguyaRobot/modules"),
     workers=min(32, os.cpu_count() + 4),
 )
 apps = []
