97934968edbbba3cfc9e5cdf3306827116a8a4d8
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-08-12 04:18:50 +0530

    Update join_request.py

--- KaguyaRobot/modules/join_request.py
+++ KaguyaRobot/modules/join_request.py
@@ -1,112 +1,42 @@
-import re
-import html
+import asyncio
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM, Message
 
-from telegram import ParseMode
-from telegram.update import Update
-from telegram.ext import ChatJoinRequestHandler
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
-from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
-from telegram.utils.helpers import mention_html
-
-from KaguyaRobot.modules.helper_funcs.anonymous import AdminPerms, user_admin
-
-from KaguyaRobot import dispatcher
-from KaguyaRobot.modules.helper_funcs.decorators import Kaguyacallback
-
-from KaguyaRobot.modules.log_channel import loggable
-
-@user_admin
-def chat_join_req(upd: Update, ctx: CallbackContext):
-    bot = ctx.bot
-    user = upd.chat_join_request.from_user
-    chat = upd.chat_join_request.chat
-    keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                            "✅ Approve", callback_data="cb_approve={}".format(user.id)
-                    ),
-                    InlineKeyboardButton(
-                            "❌ Decline", callback_data="cb_decline={}".format(user.id)
-                    ),
-                ]
-            ]
-    )
-    bot.send_message(
-            chat.id,
-            "{} wants to join {}".format(
-                    mention_html(user.id, user.first_name), chat.title or "this chat"
-            ),
-            reply_markup=keyboard,
-            parse_mode=ParseMode.HTML,
-    )
-
-
-@Kaguyacallback(pattern=r"cb_approve=")
-@user_admin(AdminPerms.CAN_INVITE_USERS, )
-@loggable
-def approve_joinReq(update: Update, context: CallbackContext) -> str:
-    bot = context.bot
-    query = update.callback_query
-    user = update.effective_user
-    chat = update.effective_chat
-    match = re.match(r"cb_approve=(.+)", query.data)
-
-    user_id = match.group(1)
-    try:
-        bot.approve_chat_join_request(chat.id, user_id)
-        joined_user = bot.get_chat_member(chat.id, user_id)
-        joined_mention = mention_html(user_id, html.escape(joined_user.user.first_name))
-        admin_mention = mention_html(user.id, html.escape(user.first_name))
-        update.effective_message.edit_text(
-                f"{joined_mention}'s join request was approved by {admin_mention}.",
-                parse_mode="HTML",
-        )
-        logmsg = (
-            f"<b>{html.escape(chat.title)}:</b>\n"
-            f"#JOIN_REQUEST\n"
-            f"Approved\n"
-            f"<b>Admin:</b> {admin_mention}\n"
-            f"<b>User:</b> {joined_mention}\n"
-        )
-        return logmsg
-    except Exception as e:
-        update.effective_message.edit_text(str(e))
-        pass
-
-
-@Kaguyacallback(pattern=r"cb_decline=")
-@user_admin(AdminPerms.CAN_INVITE_USERS, )
-@loggable
-def decline_joinReq(update: Update, context: CallbackContext) -> str:
-    bot = context.bot
-    query = update.callback_query
-    user = update.effective_user
-    chat = update.effective_chat
-    match = re.match(r"cb_decline=(.+)", query.data)
-
-    user_id = match.group(1)
-    try:
-        bot.decline_chat_join_request(chat.id, user_id)
-        joined_user = bot.get_chat_member(chat.id, user_id)
-        joined_mention = mention_html(user_id, html.escape(joined_user.user.first_name))
-        admin_mention = mention_html(user.id, html.escape(user.first_name))
-        update.effective_message.edit_text(
-                f"{joined_mention}'s join request was declined by {admin_mention}.",
-                parse_mode="HTML",
-        )
-        logmsg = (
-            f"<b>{html.escape(chat.title)}:</b>\n"
-            f"#JOIN_REQUEST\n"
-            f"Declined\n"
-            f"<b>Admin:</b> {admin_mention}\n"
-            f"<b>User:</b> {joined_mention}\n"
-        )
-        return logmsg
-    except Exception as e:
-        update.effective_message.edit_text(str(e))
-        pass
-
-
-dispatcher.add_handler(ChatJoinRequestHandler(callback=chat_join_req, run_async=True))
+@Client.on_chat_join_request()
+async def join_request_manage(client:Client, message:Message):
+    
+    user = message.from_user.id 
+    chat = message.chat.id
+    text = 'Want to join this group'
+    await client.send_message(message.chat.id, text, reply_markup=IKM([[IKB('Approve', f'wlcm_approve-{message.from_user.id}'), IKB('Decline', f'wlcm_decline-{message.from_user.id}')]]))
+     
+     
+@Client.on_callback_query(filters.regex('wlcm'))
+async def start_quer(client, callback_query):
+    if 'approve' in callback_query.data:
+        user=callback_query.data.split('-', 1)[1]
+        get_admin = await client.get_chat_member(chat_id=callback_query.message.chat.id, user_id=callback_query.from_user.id)
+        if get_admin.status == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
+            request_stats = await client.approve_chat_join_request(chat_id=int(callback_query.message.chat.id), user_id=int(user))
+            if request_stats is True: 
+                await callback_query.answer('Approved Request')
+                await asyncio.sleep(5)
+                await callback_query.message.delete()
+            else:
+                await callback_query.answer('Error While Processing Request')    
+        else:
+            await callback_query.answer('Yamete Senpai')      
+             
+    elif 'decline' in callback_query.data:
+        user=callback_query.data.split('-', 1)[1]
+        get_admin = await client.get_chat_member(chat_id=callback_query.message.chat.id, user_id=callback_query.from_user.id)
+        if get_admin.status == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
+            request_stats = await client.decline_chat_join_request(chat_id=int(callback_query.message.chat.id), user_id=int(user))
+            if request_stats is True: 
+                await callback_query.answer('Rejected Request')
+                await asyncio.sleep(5)
+                await callback_query.message.delete()
+            else:
+                await callback_query.answer('Error While Processing Request')    
+        else:
+            await callback_query.answer('Yamete Senpai')
