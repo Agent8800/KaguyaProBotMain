324e1e0b5cb6e2467b4a3d274a3ffb9f0fa251b1
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <contact.sohail.me@gmail.com>
2022-08-11 16:09:27 -0700

    Update join_request.py

--- KaguyaRobot/modules/join_request.py
+++ KaguyaRobot/modules/join_request.py
@@ -1,23 +1,23 @@
+
 import asyncio
-from pyrogram import Client, filters, enums
+from KaguyaRobot import pbot
+from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM, Message
 
-@Client.on_chat_join_request()
-async def join_request_manage(client:Client, message:Message):
-    
+@pbot.on_chat_join_request()
+async def join_request_manage(pbot:pbot, message:Message):
     user = message.from_user.id 
     chat = message.chat.id
     text = 'Want to join this group'
-    await client.send_message(message.chat.id, text, reply_markup=IKM([[IKB('Approve', f'wlcm_approve-{message.from_user.id}'), IKB('Decline', f'wlcm_decline-{message.from_user.id}')]]))
+    await pbot.send_message(message.chat.id, text, reply_markup=IKM([[IKB('Approve', f'wlcm_approve-{message.from_user.id}'), IKB('Decline', f'wlcm_decline-{message.from_user.id}')]]))
      
-     
-@Client.on_callback_query(filters.regex('wlcm'))
-async def start_quer(client, callback_query):
+@pbot.on_callback_query(filters.regex('wlcm'))
+async def start_quer(pbot, callback_query):
     if 'approve' in callback_query.data:
         user=callback_query.data.split('-', 1)[1]
-        get_admin = await client.get_chat_member(chat_id=callback_query.message.chat.id, user_id=callback_query.from_user.id)
+        get_admin = await pbot.get_chat_member(chat_id=callback_query.message.chat.id, user_id=callback_query.from_user.id)
         if get_admin.status == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
-            request_stats = await client.approve_chat_join_request(chat_id=int(callback_query.message.chat.id), user_id=int(user))
+            request_stats = await pbot.approve_chat_join_request(chat_id=int(callback_query.message.chat.id), user_id=int(user))
             if request_stats is True: 
                 await callback_query.answer('Approved Request')
                 await asyncio.sleep(5)
@@ -29,9 +29,9 @@ async def start_quer(client, callback_query):
              
     elif 'decline' in callback_query.data:
         user=callback_query.data.split('-', 1)[1]
-        get_admin = await client.get_chat_member(chat_id=callback_query.message.chat.id, user_id=callback_query.from_user.id)
+        get_admin = await pbot.get_chat_member(chat_id=callback_query.message.chat.id, user_id=callback_query.from_user.id)
         if get_admin.status == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
-            request_stats = await client.decline_chat_join_request(chat_id=int(callback_query.message.chat.id), user_id=int(user))
+            request_stats = await pbot.decline_chat_join_request(chat_id=int(callback_query.message.chat.id), user_id=int(user))
             if request_stats is True: 
                 await callback_query.answer('Rejected Request')
                 await asyncio.sleep(5)
@@ -39,4 +39,11 @@ async def start_quer(client, callback_query):
             else:
                 await callback_query.answer('Error While Processing Request')    
         else:
-            await callback_query.answer('Yamete Senpai')
+            await callback_query.answer('Yamete Senpai') 
+        
+        
+            
+        
+    
+    
+    
