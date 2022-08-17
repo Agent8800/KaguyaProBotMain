4c0e5b8a0d53167e59ad65ee732a5943bb6e069b
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-08-17 02:59:06 -0700

    Update __main__.py

--- KaguyaRobot/__main__.py
+++ KaguyaRobot/__main__.py
@@ -220,8 +220,8 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(
-                photo=(PHOTO),
-                caption=PM_START_TEXT.format(
+                PHOTO,
+                PM_START_TEXT.format(
                     escape_markdown(first_name),
                     escape_markdown(uptime),
                     sql.num_users(),
