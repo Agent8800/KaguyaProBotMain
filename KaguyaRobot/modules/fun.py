b9ecda89d06a19d0740efc2a154451602bde3d68
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-10-31 23:51:42 +0530

    Update fun.py

--- KaguyaRobot/modules/fun.py
+++ KaguyaRobot/modules/fun.py
@@ -63,7 +63,7 @@ def sanitize(update: Update, context: CallbackContext):
     reply_animation(random.choice(fun_strings.GIFS), caption=f"*Sanitizes {name}*")
 
 
-def slap(update: Update, context: CallbackContext):
+def tslap(update: Update, context: CallbackContext):
     bot, args = context.bot, context.args
     message = update.effective_message
     chat = update.effective_chat
@@ -78,7 +78,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(fun_strings.SLAP_SAITAMA_TEMPLATES)
+        temp = random.choice(fun_strings.TSLAP_SAITAMA_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
@@ -108,7 +108,7 @@ def slap(update: Update, context: CallbackContext):
         user1 = bot.first_name
         user2 = curr_user
 
-    temp = random.choice(fun_strings.SLAP_TEMPLATES)
+    temp = random.choice(fun_strings.TSLAP_TEMPLATES)
     item = random.choice(fun_strings.ITEMS)
     hit = random.choice(fun_strings.HIT)
     throw = random.choice(fun_strings.THROW)
@@ -394,7 +394,7 @@ def goodmorning(update, context):
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs, run_async=True)
-SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, run_async=True)
+TSLAP_HANDLER = DisableAbleCommandHandler("tslap", slap, run_async=True)
 PAT_HANDLER = DisableAbleCommandHandler("pat", pat, run_async=True)
 ROLL_HANDLER = DisableAbleCommandHandler("roll", roll, run_async=True)
 TOSS_HANDLER = DisableAbleCommandHandler("toss", toss, run_async=True)
@@ -419,7 +419,7 @@ dispatcher.add_handler(WEEBIFY_HANDLER)
 dispatcher.add_handler(SHOUT_HANDLER)
 dispatcher.add_handler(SANITIZE_HANDLER)
 dispatcher.add_handler(RUNS_HANDLER)
-dispatcher.add_handler(SLAP_HANDLER)
+dispatcher.add_handler(TSLAP_HANDLER)
 dispatcher.add_handler(PAT_HANDLER)
 dispatcher.add_handler(ROLL_HANDLER)
 dispatcher.add_handler(TOSS_HANDLER)
@@ -439,7 +439,7 @@ dispatcher.add_handler(GDMORNING_HANDLER)
 __mod_name__ = "Fun"
 __command_list__ = [
     "runs",
-    "slap",
+    "tslap",
     "roll",
     "toss",
     "shrug",
@@ -459,7 +459,7 @@ __command_list__ = [
 ]
 __handlers__ = [
     RUNS_HANDLER,
-    SLAP_HANDLER,
+    TSLAP_HANDLER,
     PAT_HANDLER,
     ROLL_HANDLER,
     TOSS_HANDLER,
