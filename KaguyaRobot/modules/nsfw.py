94671746a5af85703de729e2cc8e8826d566153c
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-10-31 22:16:51 +0530

    Update nsfw.py

--- KaguyaRobot/modules/nsfw.py
+++ KaguyaRobot/modules/nsfw.py
@@ -1,7 +1,20 @@
 import requests
 
-from telegram import Update
-from KaguyaRobot import dispatcher
+import os
+import html
+import nekos
+import requests
+from PIL import Image
+from telegram import ParseMode
+from KaguyaRobot import dispatcher, updater
+import KaguyaRobot.modules.sql.nsfw_sql as sql
+from KaguyaRobot.modules.log_channel import gloggable
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram.error import BadRequest, RetryAfter, Unauthorized
+from telegram.ext import CommandHandler, run_async, CallbackContext
+from KaguyaRobot.modules.helper_funcs.filters import CustomFilters
+from KaguyaRobot.modules.helper_funcs.chat_status import user_admin
+from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 def hneko(update: Update):
     m = update.effective_message
@@ -9,5 +22,5 @@ def hneko(update: Update):
     url = api["url"]
     m.reply_photo(url)
 
-hneko_handler = CommandHandler("hneko", hneko)
+hneko_handler = CommandHandler("hneko", hneko, run_async=True)
 dispatcher.add_handler(hneko_handler)
