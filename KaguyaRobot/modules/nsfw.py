9dd076898adfa7fc896c224b2d8676cadee73729
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-10-31 23:45:30 +0530

    Update nsfw.py

--- KaguyaRobot/modules/nsfw.py
+++ KaguyaRobot/modules/nsfw.py
@@ -77,3 +77,200 @@ def awoo(update: Update, context: CallbackContext):
 awoo_handler = CommandHandler("awoo", awoo)
 dispatcher.add_handler(awoo_handler)
 
+def kiss(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/kiss").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+kiss_handler = CommandHandler("kiss", kiss)
+dispatcher.add_handler(kiss_handler)
+
+def lick(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/lick").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+lick_handler = CommandHandler("lick", lick)
+dispatcher.add_handler(lick_handler)
+
+def pat(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/pat").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+pat_handler = CommandHandler("pat", pat)
+dispatcher.add_handler(pat_handler)
+
+def smug(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/smug").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+smug_handler = CommandHandler("smug", smug)
+dispatcher.add_handler(smug_handler)
+
+def bonk(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/bonk").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+bonk_handler = CommandHandler("bonk", bonk)
+dispatcher.add_handler(bonk_handler)
+
+def yeet(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/yeet").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+yeet_handler = CommandHandler("yeet", yeet)
+dispatcher.add_handler(yeet_handler)
+
+def blush(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/blush").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+blush_handler = CommandHandler("blush", blush)
+dispatcher.add_handler(blush_handler)
+
+def smile(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/smile").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+smile_handler = CommandHandler("smile", smile)
+dispatcher.add_handler(smile_handler)
+
+def wave(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/wave").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+wave_handler = CommandHandler("wave", wave)
+dispatcher.add_handler(wave_handler)
+
+def highfive(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/highfive").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+highfive_handler = CommandHandler("highfive", highfive)
+dispatcher.add_handler(highfive_handler)
+
+def handhold(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/handhold").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+handhold_handler = CommandHandler("handhold", handhold)
+dispatcher.add_handler(handhold_handler)
+
+def nom(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/nom").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+nom_handler = CommandHandler("nom", nom)
+dispatcher.add_handler(nom_handler)
+
+def bite(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/bite").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+bite_handler = CommandHandler("bite", bite)
+dispatcher.add_handler(bite_handler)
+
+def gomp(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/gomp").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+gomp_handler = CommandHandler("gomp", gomp)
+dispatcher.add_handler(gomp_handler)
+
+def slap(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/slap").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+slap_handler = CommandHandler("slap", slap)
+dispatcher.add_handler(slap_handler)
+
+def kill(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/kill").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+kill_handler = CommandHandler("kill", kill)
+dispatcher.add_handler(kill_handler)
+
+def kick(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/kick").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+kick_handler = CommandHandler("kick", kick)
+dispatcher.add_handler(kick_handler)
+
+def happy(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/happy").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+happy_handler = CommandHandler("happy", happy)
+dispatcher.add_handler(happy_handler)
+
+def wink(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/wink").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+wink_handler = CommandHandler("wink", wink)
+dispatcher.add_handler(wink_handler)
+
+def poke(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/poke").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+poke_handler = CommandHandler("poke", poke)
+dispatcher.add_handler(poke_handler)
+
+def dance(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/dance").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+dance_handler = CommandHandler("dance", dance)
+dispatcher.add_handler(dance_handler)
+
+def cringe(update: Update, context: CallbackContext):
+    m = update.effective_message
+    api = requests.get("https://api.waifu.pics/sfw/cringe").json()
+    url = api["url"]
+    m.reply_animation(animation=url)
+
+cringe_handler = CommandHandler("cringe", cringe)
+dispatcher.add_handler(cringe_handler)
