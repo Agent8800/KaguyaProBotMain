fd90f10b7194c15fa303fc39d8abac552c2b2ef6
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-11-17 22:49:35 +0530

    Update __init__.py

--- KaguyaRobot/__init__.py
+++ KaguyaRobot/__init__.py
@@ -163,6 +163,7 @@ else:
 
     JOIN_LOGGER = Config.JOIN_LOGGER
     OWNER_USERNAME = Config.OWNER_USERNAME
+    ALLOW_CHATS = Config.ALLOW_CHATS
     try:
         DRAGONS = {int(x) for x in Config.DRAGONS or []}
         DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
@@ -193,6 +194,7 @@ else:
     API_HASH = Config.API_HASH
     ERROR_LOGS = Config.ERROR_LOGS
     DB_URL = Config.SQLALCHEMY_DATABASE_URI
+    MONGO_DB_URI = Config.MONGO_DB_URI
     ARQ_API = Config.ARQ_API_KEY
     ARQ_API_URL = Config.ARQ_API_URL
     DONATION_LINK = Config.DONATION_LINK
