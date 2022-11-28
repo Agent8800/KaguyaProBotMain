5e22db7f8720733ce68eaa3a159e0817022029b8
KAC CHAN <kacxxchan@gmail.com>
2022-11-28 15:43:58 +0530

    Update __init__.py

--- KaguyaRobot/__init__.py
+++ KaguyaRobot/__init__.py
@@ -214,9 +214,6 @@ else:
     SESSION_STRING = Config.SESSION_STRING
     INFOPIC = Config.INFOPIC
     BOT_USERNAME = Config.BOT_USERNAME
-    STRING_SESSION = Config.STRING_SESSION
-    LASTFM_API_KEY = Config.LASTFM_API_KEY
-    CF_API_KEY = Config.CF_API_KEY
 
     try:
         BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
