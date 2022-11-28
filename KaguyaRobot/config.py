59e65c686c493ed156798ce29fdb4f7b0cfcc12d
KAC CHAN <kacxxchan@gmail.com>
2022-11-28 16:37:20 +0530

    Update config.py

--- KaguyaRobot/config.py
+++ KaguyaRobot/config.py
@@ -34,7 +34,7 @@ class Config(object):
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "postgres://auxaocewvksmzb:ac42fac86b5befddaca4d0b9e56790fe401697a9c5e78b5965e3b96b828cd635@ec2-35-172-98-23.compute-1.amazonaws.com:5432/d2k86p87k8j824"  # needed for any database modules
-    LOAD = ["rss"]
+    LOAD = [""]
     NO_LOAD = ["translation"]
     WEBHOOK = False
     INFOPIC = True
