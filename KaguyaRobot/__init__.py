1c4fd8248dd506b9065383b53d73c5d834766a98
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-06 17:35:44 +0530

    Update __init__.py

--- KaguyaRobot/__init__.py
+++ KaguyaRobot/__init__.py
@@ -306,7 +306,7 @@ tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
 
-REDIS_URL = "redis://:25wDxkddl8J9ncWwMT6zMzthwpklnEAd@redis-19232.c283.us-east-1-4.ec2.cloud.redislabs.com:19232"
+REDIS_URL = "redis://xelcius:Xelcius~97@redis-11262.c301.ap-south-1-1.ec2.cloud.redislabs.com:11262
 
 
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
